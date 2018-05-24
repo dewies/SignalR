61d520c53518e568edf25a7e713c91cdcc3ade86
Pranav K <prkrishn@hotmail.com>
2018-05-24 15:59:49 -0700

    Use machine affinity

--- build/buildpipeline/windows-es.groovy
+++ build/buildpipeline/windows-es.groovy
@@ -2,7 +2,7 @@
 
 // 'node' indicates to Jenkins that the enclosed block runs on a node that matches
 // the label 'windows-with-vs'
-simpleNode('Windows.10.Amd64.ClientRS4.ES') {
+simpleNode('Windows_NT', 'Windows.10.Amd64.ClientRS4.ES') {
     stage ('Checking out source') {
         checkout scm
     }
